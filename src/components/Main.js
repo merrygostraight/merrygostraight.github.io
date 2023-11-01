730865222af8bf605ff95ca58275fa50aaf35d63
Merry <majuyeonida@gmail.com>
2023-11-01 20:19:41 +0900

    clean

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import Pages from 'components/Pages';
-import Pagination from 'components/pages/Pagination';
-import { ToastContainer } from 'react-toastify';
 
 const S = {};
 S.Main = styled.div`
@@ -16,34 +13,11 @@ S.Main = styled.div`
   overflow: hidden;
 `;
 
-const pageList = [
-  { index: 1, category: '커버' },
-  { index: 2, category: '인사' },
-  { index: 3, category: '사진' },
-  { index: 4, category: '예식' },
-  { index: 5, category: '연락' },
-];
-
 function Main() {
-  const [ pageNum, setPageNum ] = useState(0);
-  const [ isShowPagination, setShowPagination ] = useState(true);
-  
-  useEffect(() => {
-    setPageNum(1);
-  }, []);
   
   return (
     <S.Main id='main'>
-      <Pages
-        pageNum={pageNum}
-        setPageNum={setPageNum}
-        lastPageNum={pageList.length}
-        showPagination={setShowPagination}
-      />
-      {isShowPagination &&
-        <Pagination pageNum={pageNum} pages={pageList} setPageNum={setPageNum} />
-      }
-      <ToastContainer />
+        HI
     </S.Main>
   );
 }
